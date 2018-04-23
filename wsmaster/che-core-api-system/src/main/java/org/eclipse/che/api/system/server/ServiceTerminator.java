c015ecda8911dadccfea4f5a9da31e973fbefd78
Max Shaposhnik <mshaposh@redhat.com>
2018-04-23 17:32:09 +0300

    Refactor terminations mechanism to support suspend and dependencies;

--- wsmaster/che-core-api-system/src/main/java/org/eclipse/che/api/system/server/ServiceTerminator.java
+++ wsmaster/che-core-api-system/src/main/java/org/eclipse/che/api/system/server/ServiceTerminator.java
@@ -10,11 +10,19 @@
  */
 package org.eclipse.che.api.system.server;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableSortedSet;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 import org.eclipse.che.api.core.notification.EventService;
 import org.eclipse.che.api.system.shared.event.service.StoppingSystemServiceEvent;
+import org.eclipse.che.api.system.shared.event.service.SuspendingSystemServiceEvent;
 import org.eclipse.che.api.system.shared.event.service.SystemServiceStoppedEvent;
+import org.eclipse.che.api.system.shared.event.service.SystemServiceSuspendedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,7 +41,8 @@ class ServiceTerminator {
   @Inject
   ServiceTerminator(EventService eventService, Set<ServiceTermination> terminations) {
     this.eventService = eventService;
-    this.terminations = terminations;
+    this.terminations =
+        ImmutableSortedSet.copyOf(new ServiceTerminationComparator(terminations), terminations);
   }
 
   /**
@@ -44,16 +53,87 @@ class ServiceTerminator {
   void terminateAll() throws InterruptedException {
     for (ServiceTermination termination : terminations) {
       LOG.info("Shutting down '{}' service", termination.getServiceName());
-      eventService.publish(new StoppingSystemServiceEvent(termination.getServiceName()));
+      doTerminate(termination);
+    }
+  }
+
+  /**
+   * Suspends system services.
+   *
+   * @throws InterruptedException when suspending is interrupted
+   */
+  void suspendAll() throws InterruptedException {
+    for (ServiceTermination termination : terminations) {
+      LOG.info("Suspending down '{}' service", termination.getServiceName());
+      eventService.publish(new SuspendingSystemServiceEvent(termination.getServiceName()));
       try {
-        termination.terminate();
+        termination.suspend();
+      } catch (UnsupportedOperationException e) {
+        LOG.info(
+            "Suspending down '{}' service ins't supported, terminating it",
+            termination.getServiceName());
+        doTerminate(termination);
       } catch (InterruptedException x) {
         LOG.error(
-            "Interrupted while waiting for '{}' service to shutdown", termination.getServiceName());
+            "Interrupted while waiting for '{}' service to suspend", termination.getServiceName());
         throw x;
       }
-      LOG.info("Service '{}' is shut down", termination.getServiceName());
-      eventService.publish(new SystemServiceStoppedEvent(termination.getServiceName()));
+      LOG.info("Service '{}' is suspended", termination.getServiceName());
+      eventService.publish(new SystemServiceSuspendedEvent(termination.getServiceName()));
+    }
+  }
+
+  @VisibleForTesting
+  void doTerminate(ServiceTermination termination) throws InterruptedException {
+    eventService.publish(new StoppingSystemServiceEvent(termination.getServiceName()));
+    try {
+      termination.terminate();
+    } catch (InterruptedException x) {
+      LOG.error(
+          "Interrupted while waiting for '{}' service to shutdown", termination.getServiceName());
+      throw x;
+    }
+    LOG.info("Service '{}' is shut down", termination.getServiceName());
+    eventService.publish(new SystemServiceStoppedEvent(termination.getServiceName()));
+  }
+
+  public static class ServiceTerminationComparator implements Comparator<ServiceTermination> {
+    public ServiceTerminationComparator(Set<ServiceTermination> terminations) {
+      this.dependencies =
+          terminations
+              .stream()
+              .collect(
+                  Collectors.toMap(
+                      ServiceTermination::getServiceName, ServiceTermination::getDependencies));
+    }
+
+    private final Map<String, Set<String>> dependencies;
+
+    @Override
+    public int compare(ServiceTermination o1, ServiceTermination o2) {
+      return checkTransitiveDependency(o1.getServiceName(), o2.getServiceName(), new HashSet<>());
+    }
+    // Recursively dig into dependencies and sort them out
+    private int checkTransitiveDependency(String o1, String o2, Set<String> loopList) {
+      if (loopList.contains(o1)) {
+        throw new RuntimeException("Circular dependency found between terminations " + loopList);
+      }
+      Set<String> directDependencies = dependencies.get(o1);
+      if (directDependencies.isEmpty()) {
+        return -1;
+      } else {
+        if (directDependencies.contains(o2)) {
+          return 1;
+        } else {
+          loopList.add(o1);
+          for (String dependency : directDependencies) {
+            if (checkTransitiveDependency(dependency, o2, loopList) > 0) {
+              return 1;
+            }
+          }
+          return -1;
+        }
+      }
     }
   }
 }
